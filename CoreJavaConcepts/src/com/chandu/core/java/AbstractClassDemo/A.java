61c760147bda3715a31a69bfa6d035871a381151
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2025-04-25 16:44:12 -0400

    CoreJavaConcepts - Abstraction Implementation

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemo/A.java
@@ -0,0 +1,17 @@
+package com.chandu.core.java.AbstractClassDemo;
+
+abstract class A
+{
+	abstract void hi();
+
+}
+
+class B extends A
+{
+	void hi()
+	{
+		System.out.println("hi...");
+	}
+
+}
+
---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemo/AbstractClassTest.java
@@ -0,0 +1,11 @@
+package com.chandu.core.java.AbstractClassDemo;
+
+public class AbstractClassTest {
+
+	public static void main(String[] args) {
+		B b = new B();
+		// This will call the Implmentation method of hi()that was defined 
+		// in Class B of the abstract method that was defined in Class A.
+		b.hi(); 	}
+
+}
