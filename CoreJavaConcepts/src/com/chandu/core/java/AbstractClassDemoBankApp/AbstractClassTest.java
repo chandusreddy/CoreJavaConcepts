e83c9fd384f9c2b71a566b17b8ab9a8cd0dae8b2
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2025-04-26 09:42:19 -0400

    CoreJavaConcepts - Abstraction Banking Example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoBankApp/AbstractClassTest.java
@@ -0,0 +1,15 @@
+package com.chandu.core.java.AbstractClassDemoBankApp;
+
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		Bank bankRef = new HDFC();
+		System.out.println("HDFC Bank : "+bankRef.getRateOfInterest());
+		
+		bankRef = new ICICI();
+		System.out.println("ICICI Bank : "+bankRef.getRateOfInterest());
+	}
+
+}
---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoBankApp/Bank.java
@@ -0,0 +1,28 @@
+package com.chandu.core.java.AbstractClassDemoBankApp;
+
+public abstract class Bank
+{
+	abstract int getRateOfInterest();
+}
+
+class HDFC extends Bank
+{
+
+	@Override
+	int getRateOfInterest()
+	{
+		return 10;
+	}
+	
+}
+
+class ICICI extends Bank
+{
+
+	@Override
+	int getRateOfInterest()
+	{
+		return 7;
+	}
+	
+}
