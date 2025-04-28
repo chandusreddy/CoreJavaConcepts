fe8b21da4807ee24c40003570130c29341dfb629
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2025-04-28 10:13:47 -0400

    CoreJavaConcepts - Abstraction Bike Example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoBikeApp/AbstractClassTest.java
@@ -0,0 +1,13 @@
+package com.chandu.core.java.AbstractClassDemoBikeApp;
+
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		Bike bikeRef = new Honda();
+		bikeRef.run();
+		bikeRef.changeGear();
+	}
+
+}
