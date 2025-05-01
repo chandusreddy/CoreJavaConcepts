099f3265a8493cda634bbab72db94fdaa9aa0a8f
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-01 14:02:47 -0400

    CoreJavaConcepts - Abstraction Method

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoNormalMethodApp/A.java
@@ -0,0 +1,21 @@
+package com.chandu.core.java.AbstractClassDemoNormalMethodApp;
+abstract class A
+{
+	abstract void hi();
+	
+	void bye()
+	{
+		System.out.println("Bye...");
+	}
+
+}
+
+class B extends A
+{
+	void hi()
+	{
+		System.out.println("hi...");
+	}
+
+}
+
---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoNormalMethodApp/AbstractClassTest.java
@@ -0,0 +1,11 @@
+package com.chandu.core.java.AbstractClassDemoNormalMethodApp;
+
+public class AbstractClassTest {
+
+	public static void main(String[] args) {
+		B b = new B();
+		b.hi();
+		b.bye();
+	}
+
+}
