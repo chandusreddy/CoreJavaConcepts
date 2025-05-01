1f70c0e265e6a1dc80edf0d6c30005ae6fe44488
Chandu0005 <70282603+Chandu0005@users.noreply.github.com>
2025-05-01 13:23:07 -0400

    CoreJavaConcepts - Abstraction Interface Example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoInterfaceImpl/AbstractClassTest.java
@@ -0,0 +1,15 @@
+package com.chandu.core.java.AbstractClassDemoInterfaceImpl;
+
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		A aRef = new MyClass();
+		aRef.sayWelcome();
+		aRef.sayHi();
+		aRef.sayBye();
+		
+	}
+
+}
