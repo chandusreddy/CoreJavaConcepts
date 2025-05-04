bd68660d49adc5bcf6925119abcb4e72d2fa0a93
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-04 17:39:24 -0400

    CoreJavaConcepts - Abstraction Static App

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoStaticApp/AbstractClassTest.java
@@ -0,0 +1,13 @@
+package com.chandu.core.java.AbstractClassDemoStaticApp;
+
+
+public class AbstractClassTest
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("Number of Legs = "+Person.numberOfLegs);
+		Person.eat();		
+	}
+
+}
