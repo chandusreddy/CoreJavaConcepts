67a12fbcf9285dfcd52b72e692b98f9c217e0f88
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-08 10:44:10 -0400

    CoreJavaConcepts - Abstraction Class Example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoClassApp/mypack1/Student.java
@@ -0,0 +1,7 @@
+package com.chandu.core.java.AbstractClassDemoClassApp.mypack1;
+
+public class Student
+{
+	private String name;
+
+}
---
+++ CoreJavaConcepts/src/com/chandu/core/java/AbstractClassDemoClassApp/mypack2/StudentMyPack2Test.java
@@ -0,0 +1,12 @@
+package com.chandu.core.java.AbstractClassDemoClassApp.mypack2;
+
+import com.chandu.core.java.AbstractClassDemoClassApp.mypack1.Student;
+
+public class StudentMyPack2Test {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		System.out.println("student : " + student);
+	}
+
+}
