01ced256cf0166942dd8106699f4ab94d3cfc3ea
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-14 10:41:16 -0400

    CoreJavaConcepts - Access Modifiers Getter/Setter App

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AccessModifiersDemoGetterSetterApp/Student.java
@@ -0,0 +1,33 @@
+package com.chandu.core.java.AccessModifiersDemoGetterSetterApp;
+
+public class Student {
+
+	/*
+	 * private modifier�the field is accessible only within its own class.
+	 * 
+	 * In the spirit of encapsulation, it is common to make fields private. This
+	 * means that they can only be directly accessed from the Student class. We
+	 * still need access to these values, however. This can be done indirectly by
+	 * adding public methods that obtain the field values for us:
+	 */
+
+	private String name;
+	private int age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ CoreJavaConcepts/src/com/chandu/core/java/AccessModifiersDemoGetterSetterApp/StudentDemoTest.java
@@ -0,0 +1,14 @@
+package com.chandu.core.java.AccessModifiersDemoGetterSetterApp;
+
+public class StudentDemoTest {
+
+	public static void main(String[] args) {
+		Student student = new Student();
+		student.setName("Dave");
+		student.setAge(23);
+
+		System.out.println("Name : " + student.getName());
+		System.out.println("Age  : " + student.getAge());
+	}
+
+}
