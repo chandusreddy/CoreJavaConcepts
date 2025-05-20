526a1435f7817c076b25a17bc95c7f67b1234795
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-20 10:23:04 -0400

    CoreJavaConcepts - Annotations Deprecated example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AnnotationDemoDepcreatedApp/AnnotationsDemo.java
@@ -0,0 +1,15 @@
+package com.chandu.core.java.AnnotationDemoDepcreatedApp;
+
+class Animal {
+	@Deprecated
+	void eatChicken() {
+		System.out.println("Eating 2 chicken legs");
+	}
+}
+
+public class AnnotationsDemo {
+	public static void main(String args[]) {
+		Animal animal = new Animal();
+		animal.eatChicken();
+	}
+}
