a2fc857222475d35be948ec7111e31ccb1ea7844
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-23 09:50:45 -0400

    CoreJavaConcepts - Annotation Introduction app

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AnnotationDemoIntroApp/AnnotationsDemo.java
@@ -0,0 +1,22 @@
+package com.chandu.core.java.AnnotationDemoIntroApp;
+
+import java.util.ArrayList;
+
+public class AnnotationsDemo {
+
+	/*
+	 * If you remove the @SuppressWarnings("unchecked") annotation, it will show
+	 * warning at compile time because we are using non-generic collection.
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String args[]) {
+		ArrayList list = new ArrayList();
+		list.add("Ram");
+		list.add("Peter");
+
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+
+	}
+}
