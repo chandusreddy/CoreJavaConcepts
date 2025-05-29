3736bd740e1711abef69884d215eb635f5c3f8f5
chandra shekar palle <cshekarpalle@gmail.com>
2025-05-29 09:50:19 -0400

    CoreJavaConcepts - Annotations Repeating example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/AnnotationDemoRepeatingSchApp/RepeatingAnnotations.java
@@ -0,0 +1,19 @@
+package com.chandu.core.java.AnnotationDemoRepeatingSchApp;
+
+public class RepeatingAnnotations {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Retrieving Annotations using Reflection API method
+		 */
+		Schedules schedules = ServerRestartSchedule.class.getAnnotation(Schedules.class);
+		System.out.println(schedules);
+		Schedule[] scheduleArray = schedules.value();
+		for (Schedule schedule : scheduleArray) {
+			System.out.println(schedule + " , dayOfMonth = " + schedule.dayOfMonth() + " , dayOfWeek = "
+					+ schedule.dayOfWeek() + " , hour = " + schedule.hour());
+		}
+
+	}
+}
