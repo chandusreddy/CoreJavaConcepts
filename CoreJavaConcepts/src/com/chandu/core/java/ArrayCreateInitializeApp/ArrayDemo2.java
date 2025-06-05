5ce8c215ce61cd6947f2372f96f201205eddfde8
chandra shekar palle <cshekarpalle@gmail.com>
2025-06-05 09:47:47 -0400

    CoreJavaConcepts - Array Initialization Example

---
+++ CoreJavaConcepts/src/com/chandu/core/java/ArrayCreateInitializeApp/ArrayDemo2.java
@@ -0,0 +1,14 @@
+package com.chandu.core.java.ArrayCreateInitializeApp;
+
+public class ArrayDemo2 {
+	public static void main(String[] args) {
+		int[] intArray = { 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
+
+		int i = 0;
+		for (int element : intArray) {
+			System.out.println("Element at Index " + i + " : " + element);
+			++i;
+		}
+
+	}
+}
