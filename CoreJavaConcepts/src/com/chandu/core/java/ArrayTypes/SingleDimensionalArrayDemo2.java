30260aaef082c3dc0ae641e948fb6de973ba4d29
chandra shekar palle <cshekarpalle@gmail.com>
2025-06-12 10:38:39 -0400

    CoreJavaConcepts - MultiDimensionalArray Initialization

---
+++ CoreJavaConcepts/src/com/chandu/core/java/ArrayTypes/SingleDimensionalArrayDemo2.java
@@ -0,0 +1,18 @@
+package com.chandu.core.java.ArrayTypes;
+
+public class SingleDimensionalArrayDemo2
+{
+
+	public static void main(String[] args)
+	{
+		int intArray[] =
+		{ 51, 61, 89 };// declaration, instantiation and initialization
+
+		for (int i = 0; i < intArray.length; i++)
+		{
+			System.out.println("Element at index position " + i + " : "
+					+ intArray[i]);
+		}
+	}
+
+}
