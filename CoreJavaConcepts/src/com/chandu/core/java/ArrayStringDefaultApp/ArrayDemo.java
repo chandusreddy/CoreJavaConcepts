c2d53aead1f6c386012d19ac4f1f703caf756f5b
chandra shekar palle <cshekarpalle@gmail.com>
2025-06-21 11:56:20 -0400

    CoreJavaConcepts - String Array App

---
+++ CoreJavaConcepts/src/com/chandu/core/java/ArrayStringDefaultApp/ArrayDemo.java
@@ -0,0 +1,16 @@
+package com.chandu.core.java.ArrayStringDefaultApp;
+
+class ArrayDemo {
+	public static void main(String[] args) {
+		/*
+		 * Declares an array of strings and allocates memory for 2 strings.
+		 */
+		String[] stringArray = new String[2];
+
+		int i = 0;
+		for (String element : stringArray) {
+			System.out.println("Element at Index " + i + " : " + element);
+			++i;
+		}
+	}
+}
