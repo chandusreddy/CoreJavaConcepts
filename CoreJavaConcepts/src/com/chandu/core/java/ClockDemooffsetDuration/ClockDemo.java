4eab2aea371f8f38664b2de4d325684a478f76da
chandra shekar palle <cshekarpalle@gmail.com>
2025-07-05 12:11:08 -0400

    CoreJavaConcepts - Clock offsetDuration

---
+++ CoreJavaConcepts/src/com/chandu/core/java/ClockDemooffsetDuration/ClockDemo.java
@@ -0,0 +1,35 @@
+package com.chandu.core.java.ClockDemooffsetDuration;
+
+import java.time.Clock;
+import java.time.Duration;
+
+public class ClockDemo
+{
+
+	public static void main(String[] args)
+	{
+		Clock baseClock = Clock.systemUTC();
+		System.out.println("instant of baseClock = " + baseClock.instant());
+
+		Duration duration = Duration.ofHours(6);
+		System.out.println("duration = " + duration);
+
+		/*
+		 * Parameters:
+		 * 
+		 * baseClock - the base clock to add the duration to, not null
+		 * offset
+		 * 
+		 * Duration - the duration to add, not null
+		 * 
+		 * Returns:
+		 * 
+		 * a clock based on the base clock with the duration added,
+		 * not null
+		 */
+
+		Clock clock = Clock.offset(baseClock, duration);
+		System.out.println("instant of clock     = " + clock.instant());
+	}
+
+}
