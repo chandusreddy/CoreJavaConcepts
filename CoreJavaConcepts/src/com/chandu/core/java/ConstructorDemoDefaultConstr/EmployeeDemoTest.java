354ce89fdb8115382f27c8d200420c8b661f4acf
chandra shekar palle <cshekarpalle@gmail.com>
2025-08-01 07:18:01 -0400

    CoreJavaConcepts - Default Constructor

---
+++ CoreJavaConcepts/src/com/chandu/core/java/ConstructorDemoDefaultConstr/EmployeeDemoTest.java
@@ -0,0 +1,18 @@
+package com.chandu.core.java.ConstructorDemoDefaultConstr;
+
+public class EmployeeDemoTest
+{
+
+	public static void main(String[] args)
+	{
+		/*
+		 * Invokes the no-argument constructor to create a new Employee object
+		 * called employee.
+		 */
+		Employee employee = new Employee();
+		System.out.println("Name : " + employee.name);
+		System.out.println("Age  : " + employee.age);
+
+	}
+
+}
